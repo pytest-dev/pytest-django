953c236b35a0cec18b86743f484f884b2eb6dc20
Andreas Pelme <andreas@pelme.se>
2012-05-01 16:45:57 +0200

    Bump version to 1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name='pytest-django',
-    version='1.0.0',
+    version='1.1',
     description='A Django plugin for py.test.',
     author='Andreas Pelme',
     author_email='andreas@pelme.se',
