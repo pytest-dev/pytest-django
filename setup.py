dd11cce929b3cadeabf0447af5a3ebe9016191f5
Andreas Pelme <andreas@pelme.se>
2012-05-01 17:08:03 +0200

    Trying to make the PyPI page a bit nicer..

--- setup.py
+++ setup.py
@@ -1,18 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import setup
+
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
     name='pytest-django',
-    version='1.1',
+    version='1.0.1',
     description='A Django plugin for py.test.',
     author='Andreas Pelme',
     author_email='andreas@pelme.se',
     maintainer="Andreas Pelme",
     maintainer_email="andreas@pelme.se",
-    url='https://github.com/pelme/pytest_django',
+    url='http://pytest-django.readthedocs.org/',
     packages=['pytest_django'],
+    long_description=read('README.rst'),
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Framework :: Django',
                  'Intended Audience :: Developers',
@@ -21,4 +31,4 @@ setup(
                  'Programming Language :: Python',
                  'Topic :: Software Development :: Testing'],
     # the following makes a plugin available to py.test
-    entry_points=dict(pytest11=['django = pytest_django']))
+    entry_points={'pytest11': ['django = pytest_django']})
