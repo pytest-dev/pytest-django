66eadda6a2a26cfef2f38449736183b8b5175022
Andreas Pelme <andreas@pelme.se>
2012-05-16 09:05:24 +0200

    Make module imports 2.5 compatible

--- pytest_django/__init__.py
+++ pytest_django/__init__.py
@@ -1,3 +1,8 @@
-from .plugin import *
-from .funcargs import *
-from .marks import *
+from pytest_django.plugin import *
+from pytest_django.funcargs import *
+from pytest_django.marks import *
+
+# When Python 2.5 support is dropped, these imports can be used instead:
+# from .plugin import *
+# from .funcargs import *
+# from .marks import *
