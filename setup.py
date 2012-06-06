9326f3049150d11aab0fc121c179f6a085166edd
Andreas Pelme <andreas@pelme.se>
2012-06-06 11:17:36 +0200

    Version bump for new pypi upload

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(fname):
 
 setup(
     name='pytest-django',
-    version='1.3',
+    version='1.3.1',
     description='A Django plugin for py.test.',
     author='Andreas Pelme',
     author_email='andreas@pelme.se',
