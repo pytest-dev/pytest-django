e71c4a94015b3b1e80dc5656449b239a62208ca7
Floris Bruynooghe <flub@devork.be>
2012-11-12 23:06:45 +0000

    Cleanup tests
    Remove unused code and make use of some more testdir features.  Also
    make the intend a bit clearer by relying on less constants and
    substitutions.  This makes for a little more typing but is still
    succinct and makes things readable by all information being together.

--- tests/test_django_settings_module.py
+++ tests/test_django_settings_module.py
@@ -1,4 +1,7 @@
-import pytest
+"""Tests which check the various ways you can set DJANGO_SETTINGS_MODULE
+
+If these tests fail you probably forgot to run "python setup.py develop".
+"""
 
 BARE_SETTINGS = '''
 # At least one database must be configured
@@ -8,111 +11,66 @@ DATABASES = {
         'NAME': ':memory:'
     },
 }
-
-'''
-# This test is a bit meta. :)
-DJANGO_SETTINGS_MODULE_TEST = '''
-import os
-
-def test_django_settings_module():
-    assert os.environ['DJANGO_SETTINGS_MODULE'] == 'tpkg.%s'
 '''
 
-PYTEST_INI = '''
-[pytest]
-DJANGO_SETTINGS_MODULE = tpkg.%s
-'''
 
 def test_ds_env(testdir, monkeypatch):
-    SETTINGS_NAME = 'settings_env'
+    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'tpkg.settings_env')
+    pkg = testdir.mkpydir('tpkg')
+    settings = pkg.join('settings_env.py')
+    settings.write(BARE_SETTINGS)
+    testdir.makepyfile("""
+        import os
 
-    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'tpkg.%s' % SETTINGS_NAME)
-    path = testdir.mkpydir('tpkg')
-    path.join('%s.py' % SETTINGS_NAME).write(BARE_SETTINGS)
-    path.join("test_ds.py").write(DJANGO_SETTINGS_MODULE_TEST % SETTINGS_NAME)
+        def test_settings():
+            assert os.environ['DJANGO_SETTINGS_MODULE'] == 'tpkg.settings_env'
+    """)
+    result = testdir.runpytest()
+    result.stdout.fnmatch_lines(['*1 passed*'])
 
-    result = testdir.runpytest('')
-    result.stdout.fnmatch_lines([
-        "*1 passed*",
-    ])
-
-    assert result.ret == 0
-
-
-# def test_ds_ini(testdir, monkeypatch):
-#     path = testdir.mkpydir('tpkg')
-
-#     SETTINGS_NAME = 'settings_ini'
-
-#     # Should be ignored
-#     monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'DO_NOT_USE')
-
-
-#     testdir.tmpdir.join('pytest.ini').write(PYTEST_INI % SETTINGS_NAME)
-#     path.join('%s.py' % SETTINGS_NAME).write(BARE_SETTINGS)
-#     path.join("test_ds.py").write(DJANGO_SETTINGS_MODULE_TEST % SETTINGS_NAME)
-
-#     result = testdir.runpytest('')
-#     result.stdout.fnmatch_lines([
-#         "*1 passed*",
-#     ])
-
-#     assert result.ret == 0
 
 def test_ds_ini(testdir, monkeypatch):
-    pkg = testdir.mkpydir('tpkg')
     monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'DO_NOT_USE')
     testdir.makeini("""\
        [pytest]
        DJANGO_SETTINGS_MODULE = tpkg.settings_ini
     """)
+    pkg = testdir.mkpydir('tpkg')
     settings = pkg.join('settings_ini.py')
     settings.write(BARE_SETTINGS)
     testdir.makepyfile("""
-       import os
+        import os
 
-       def test_ds(pytestconfig):
-           print 'xxx', repr(pytestconfig.option.ds)
-           print 'xxx', repr(os.environ.get('DJANGO_SETTINGS_MODULE'))
-           print 'xxx', repr(pytestconfig.getini('DJANGO_SETTINGS_MODULE'))
-           assert os.environ['DJANGO_SETTINGS_MODULE'] == 'tpkg.settings_ini'
+        def test_ds():
+            assert os.environ['DJANGO_SETTINGS_MODULE'] == 'tpkg.settings_ini'
     """)
-    # testdir.plugins = ['pytest_django']
     result = testdir.runpytest()
     result.stdout.fnmatch_lines(['*1 passed*'])
 
 
 def test_ds_option(testdir, monkeypatch):
-    path = testdir.mkpydir('tpkg')
+    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'DO_NOT_USE_env')
+    testdir.makeini("""\
+       [pytest]
+       DJANGO_SETTINGS_MODULE = DO_NOT_USE_ini
+    """)
+    pkg = testdir.mkpydir('tpkg')
+    settings = pkg.join('settings_opt.py')
+    settings.write(BARE_SETTINGS)
+    testdir.makepyfile("""
+        import os
 
-    SETTINGS_NAME = 'settings_option'
-
-    # Should be ignored
-    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'DO_NOT_USE_1')
-    testdir.tmpdir.join('pytest.ini').write(PYTEST_INI % 'DO_NOT_USE_2')
-    path.join('%s.py' % SETTINGS_NAME).write(BARE_SETTINGS)
-
-    path.join("test_ds.py").write(DJANGO_SETTINGS_MODULE_TEST % SETTINGS_NAME)
-
-    result = testdir.runpytest('--ds=tpkg.%s' % SETTINGS_NAME)
-    result.stdout.fnmatch_lines([
-        "*1 passed*",
-    ])
-
-    assert result.ret == 0
+        def test_ds():
+            assert os.environ['DJANGO_SETTINGS_MODULE'] == 'tpkg.settings_opt'
+    """)
+    result = testdir.runpytest('--ds=tpkg.settings_opt')
+    result.stdout.fnmatch_lines(['*1 passed*'])
 
 
 def test_ds_non_existent(testdir, monkeypatch):
-    """
-    Make sure we do not fail with INTERNALERROR if an incorrect
-    DJANGO_SETTINGS_MODULE is given.
-    """
-    path = testdir.mkpydir('tpkg')
-
+    # Make sure we do not fail with INTERNALERROR if an incorrect
+    # DJANGO_SETTINGS_MODULE is given.
     monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'DOES_NOT_EXIST')
-    path.join("test_foo.py").write('def test_foo(): pass')
-
-    result = testdir.runpytest('')
-    result.stderr.fnmatch_lines([
-        "ERROR: Could not import settings 'DOES_NOT_EXIST' (Is it on sys.path?): No module named DOES_NOT_EXIST",
-    ])
+    testdir.makepyfile('def test_ds(): pass')
+    result = testdir.runpytest()
+    result.stderr.fnmatch_lines(['*ERROR*DOES_NOT_EXIST*'])
