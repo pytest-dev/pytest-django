784f7079759a123cede86692f8683f6c51ab4769
Floris Bruynooghe <flub@devork.be>
2012-11-22 23:41:05 +0000

    Small cleanup on for the pytest.mark.url tests

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,16 +1,17 @@
+import pytest
+from django.conf import settings
+
 try:
     from django.core.urlresolvers import is_valid_path
-    is_valid_path  # Avoid pyflakes warning
 except ImportError:
     from django.core.urlresolvers import resolve, Resolver404
 
     def is_valid_path(path, urlconf=None):
-        """
-        Returns True if the given path resolves against the default URL resolver,
-        False otherwise.
+        """Return True if path resolves against default URL resolver
 
-        This is a convenience method to make working with "is this a match?" cases
-        easier, avoiding unnecessarily indented try...except blocks.
+        This is a convenience method to make working with "is this a
+        match?" cases easier, avoiding unnecessarily indented
+        try...except blocks.
         """
         try:
             resolve(path, urlconf)
@@ -19,21 +20,13 @@ except ImportError:
             return False
 
 
-from django.conf import settings
-
-import pytest
-
-
 @pytest.mark.urls('tests.urls_overridden')
-def test_urls(client):
+def test_urls():
     assert settings.ROOT_URLCONF == 'tests.urls_overridden'
     assert is_valid_path('/overridden_url/')
 
-    response = client.get('/overridden_url/')
-
-    assert response.content == 'Overridden urlconf works!'
-
 
 @pytest.mark.urls('tests.urls_overridden')
-def test_urls_mark(client):
-    test_urls(client)
+def test_urls_client(client):
+    response = client.get('/overridden_url/')
+    assert response.content == 'Overridden urlconf works!'
