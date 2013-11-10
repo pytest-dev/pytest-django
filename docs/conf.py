3d255369f2c1f64f63e2460402b5ceeae3657bd4
Andreas Pelme <andreas@pelme.se>
2013-11-10 09:30:07 +0100

    enable new Read the Docs theme

--- docs/conf.py
+++ docs/conf.py
@@ -95,6 +95,7 @@ pygments_style = 'sphinx'
 # a list of builtin themes.
 html_theme = 'default'
 html_style = 'rtd.css'
+RTD_NEW_THEME = True
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
