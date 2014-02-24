f3d4ecf7dab506861f41699023b6140c8f829bc9
Alexander Karev <alexandr.karev@ostrovok.ru>
2014-02-25 00:19:00 +0400

    tests improve

--- tests/test_db_name.py
+++ tests/test_db_name.py
@@ -3,17 +3,6 @@
 from pytest_django.db_reuse import _get_db_name
 
 
-def test_testname():
-    db_settings = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pytest_django',
-        'TEST_NAME': 'test123',
-        'HOST': 'localhost',
-        'USER': '',
-    }
-    assert _get_db_name(db_settings, None) == 'test123'
-
-
 def test_name():
     db_settings = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
@@ -23,9 +12,10 @@ def test_name():
         'USER': '',
     }
     assert _get_db_name(db_settings, None) == 'test_pytest_django'
+    assert _get_db_name(db_settings, 'abc') == 'test_pytest_django_abc'
 
 
-def test_testname_with_suffix():
+def test_testname():
     db_settings = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'pytest_django',
@@ -33,15 +23,5 @@ def test_testname_with_suffix():
         'HOST': 'localhost',
         'USER': '',
     }
+    assert _get_db_name(db_settings, None) == 'test123'
     assert _get_db_name(db_settings, 'abc') == 'test123_abc'
-
-
-def test_name_with_suffix():
-    db_settings = {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pytest_django',
-        'TEST_NAME': '',
-        'HOST': 'localhost',
-        'USER': '',
-    }
-    assert _get_db_name(db_settings, 'abc') == 'test_pytest_django_abc'
