79908fff2b1f4f581a12252fe2180bccb82b1fc2
Alexander Karev <alexandr.karev@ostrovok.ru>
2014-02-24 23:59:26 +0400

    tests for _get_test_db_name

---
+++ tests/test_db_name.py
@@ -0,0 +1,47 @@
+# coding: utf-8
+
+from pytest_django.db_reuse import _get_db_name
+
+
+def test_testname():
+    db_settings = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pytest_django',
+        'TEST_NAME': 'test123',
+        'HOST': 'localhost',
+        'USER': '',
+    }
+    assert _get_db_name(db_settings, None) == 'test123'
+
+
+def test_name():
+    db_settings = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pytest_django',
+        'TEST_NAME': '',
+        'HOST': 'localhost',
+        'USER': '',
+    }
+    assert _get_db_name(db_settings, None) == 'test_pytest_django'
+
+
+def test_testname_with_suffix():
+    db_settings = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pytest_django',
+        'TEST_NAME': 'test123',
+        'HOST': 'localhost',
+        'USER': '',
+    }
+    assert _get_db_name(db_settings, 'abc') == 'test123_abc'
+
+
+def test_name_with_suffix():
+    db_settings = {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'pytest_django',
+        'TEST_NAME': '',
+        'HOST': 'localhost',
+        'USER': '',
+    }
+    assert _get_db_name(db_settings, 'abc') == 'test_pytest_django_abc'
