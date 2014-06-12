f85d1351cd16397d21679d6506b1769b4704d6ec
Daniel Hahler <git@thequod.de>
2014-06-12 02:57:21 +0200

    Add failing tests: test_ds_after_user_conftest

--- tests/test_django_settings_module.py
+++ tests/test_django_settings_module.py
@@ -78,6 +78,38 @@ def test_ds_non_existent(testdir, monkeypatch):
         ["*Could not import settings 'DOES_NOT_EXIST' (Is it on sys.path?*):*"])
 
 
+def test_ds_after_user_conftest(testdir, monkeypatch):
+    """
+    Test that the settings module can be imported, after pytest has adjusted
+    the sys.path.
+    """
+    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'settings_after_conftest')
+    testdir.makepyfile('def test_ds(): pass')
+    testdir.makepyfile(settings_after_conftest="SECRET_KEY='secret'")
+    # testdir.makeconftest("import sys; print(sys.path)")
+    result = testdir.runpytest('-v')
+    result.stdout.fnmatch_lines(['*1 passed*'])
+
+
+def test_ds_after_user_conftest_subdir(testdir, monkeypatch):
+    """
+    Test that the settings module can be imported, after pytest has adjusted
+    the sys.path according to the conftest module (in a subdirectory).
+    """
+    monkeypatch.setenv('DJANGO_SETTINGS_MODULE', 'settings.after_conftest')
+
+    testdir.mkdir('project')
+    testdir.mkdir('project/src').join('conftest.py').write(
+        'import sys; print(sys.path)', ensure=True)
+    testdir.mkpydir('project/src/settings').join('after_conftest.py').write(
+        "SECRET_KEY='secret'")
+    testdir.tmpdir.join('project/tests').join('test_tests.py').write(
+        'def test_ds(): pass', ensure=True)
+
+    result = testdir.runpytest('-v')
+    result.stdout.fnmatch_lines(['*1 passed*'])
+
+
 def test_django_settings_configure(testdir, monkeypatch):
     """
     Make sure Django can be configured without setting
