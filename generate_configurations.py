90d13f7f8de2aeab0e08b76da09818a8cd0f639f
Daniel Hahler <git@thequod.de>
2014-06-13 15:04:06 +0200

    Add "sqlite_file" to settings for tox/travis

--- generate_configurations.py
+++ generate_configurations.py
@@ -11,7 +11,7 @@ TestEnv = namedtuple('TestEnv', ['python_version', 'django_version', 'settings']
 
 PYTHON_VERSIONS = ['python2.6', 'python2.7', 'python3.2', 'python3.3', 'python3.4', 'pypy']
 DJANGO_VERSIONS = ['1.3', '1.4', '1.5', '1.6', '1.7', 'master']
-SETTINGS = ['sqlite', 'mysql_myisam', 'mysql_innodb', 'postgres']
+SETTINGS = ['sqlite', 'sqlite_file', 'mysql_myisam', 'mysql_innodb', 'postgres']
 DJANGO_REQUIREMENTS = {
     '1.3': 'Django==1.3.7',
     '1.4': 'Django==1.4.11',
