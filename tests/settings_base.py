b54be0d6a368dbfa47a0d6beb1ded4264399bd76
Andreas Pelme <andreas@pelme.se>
2014-07-02 21:03:40 +0200

    Remove the XFrameOptionsMiddleware from settings

--- tests/settings_base.py
+++ tests/settings_base.py
@@ -29,5 +29,4 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
