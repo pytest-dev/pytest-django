e6a1e9670e857119c7e6c9250849ee4edd026bad
Andreas Pelme <andreas@pelme.se>
2014-07-02 20:32:02 +0200

    Make sure AuthenticationMiddleware is defined in settings during tests.

--- tests/settings_base.py
+++ tests/settings_base.py
@@ -22,3 +22,12 @@ if uid:
     db_suffix = '_%s' % uid
 else:
     db_suffix = ''
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
