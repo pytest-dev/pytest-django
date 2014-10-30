a592a01f44b3c2cdd944565f300276db890c92bd
Paul Collins <paul.collins.iii@gmail.com>
2014-10-30 11:29:46 -0700

    Add failing test for is_unitttest
    This is a bit ugly having to test this via the
    pytest_collection_modifyitems, but I can't think of another way to test
    this.

---
+++ tests/test_django_compat.py
@@ -0,0 +1,17 @@
+import pytest
+
+from pytest_django.django_compat import is_django_unittest
+
+from django.test import TestCase
+
+@pytest.fixture(scope="session", autouse=True)
+def check_is_django_unitttest(request):
+    for item in request.node.items:
+        if hasattr(item, 'cls') and item.cls == TestSimple:
+            assert is_django_unittest(item)
+
+
+class TestSimple(TestCase):
+    def test_nothing(self):
+        pass
+
