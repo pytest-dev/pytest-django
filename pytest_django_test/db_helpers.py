db54a1368b569b32e0c4281d8dc84790d5d14507
Daniel Hahler <git@thequod.de>
2015-04-27 20:17:52 +0200

    Fix pep8: W503: line break before binary operator

--- pytest_django_test/db_helpers.py
+++ pytest_django_test/db_helpers.py
@@ -95,8 +95,8 @@ def drop_database(name=TEST_DB_NAME, suffix=None):
 
     if get_db_engine() == 'mysql':
         r = run_mysql('-e', 'DROP DATABASE %s' % name)
-        assert ('database doesn\'t exist' in force_text(r.std_err)
-                or r.status_code == 0)
+        assert ('database doesn\'t exist' in force_text(r.std_err) or
+                r.status_code == 0)
         return
 
     if get_db_engine() == 'sqlite3':
