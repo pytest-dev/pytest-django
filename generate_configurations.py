cf6b5e5c4e93ff1e42d7b0a04ca941979a6ba400
Daniel Hahler <git@thequod.de>
2015-05-09 16:19:31 +0200

    tox.ini: use {posargs:tests} (defaults to tests)

--- generate_configurations.py
+++ generate_configurations.py
@@ -110,7 +110,7 @@ def commands(uid, env):
         yield 'sh -c "dropdb %(name)s;' \
             ' createdb %(name)s || exit 0"' % {'name': db_name}
 
-    yield 'py.test --ds=pytest_django_test.settings_%s --strict -r fEsxXw {posargs}' % env.settings
+    yield 'py.test --ds=pytest_django_test.settings_%s --strict -r fEsxXw {posargs:tests}' % env.settings
 
 
 def testenv_name(env):
