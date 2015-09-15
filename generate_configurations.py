ff810f2cf5dacb0d66e25f145d69ad07dc5d5180
Daniel Hahler <git@thequod.de>
2015-09-15 15:15:58 +0200

    config: add Python 3.5

--- generate_configurations.py
+++ generate_configurations.py
@@ -26,7 +26,7 @@ class TestEnv(TestEnvBase):
 RUN_PYTHON = '3.4'
 PYTHON_MAIN_VERSIONS = ['python2.7', 'python3.4']
 PYTHON_VERSIONS = ['python2.6', 'python2.7', 'python3.2', 'python3.3',
-                   'python3.4', 'pypy', 'pypy3']
+                   'python3.4', 'python3.5', 'pypy', 'pypy3']
 PYTEST_VERSIONS = ['2.7.2']
 DJANGO_VERSIONS = ['1.3', '1.4', '1.5', '1.6', '1.7', '1.8', 'master']
 SETTINGS = ['sqlite', 'sqlite_file', 'mysql_myisam', 'mysql_innodb',
@@ -78,7 +78,7 @@ def is_valid_env(env):
         env.django_version in ('1.7', '1.8', '1.9', 'master')):
         return False
 
-    # pypy3 is compatible with Python 3.2, but Django 1.9 only supports Python 2.7, 3.4, 3.5
+    # pypy3 is compatible with Python 3.2, but Django 1.9 only supports Python 2.7, 3.4+.
     if env.python_version == 'pypy3' and env.django_version in ('1.9', 'master'):
         return False
 
