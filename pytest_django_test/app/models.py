d46674301321fa2b233be057f92f908cf89dbcda
Jakub Owczarski <jakub3279@gmail.com>
2015-09-24 12:26:42 +0000

    created django_use_model marker for testing unmanaged models

--- pytest_django_test/app/models.py
+++ pytest_django_test/app/models.py
@@ -3,3 +3,10 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
+
+
+class Unmanaged(models.Model):
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
