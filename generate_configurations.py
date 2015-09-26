91260f0ce7aeab6d118c5c108c95877581a2d276
Andreas Pelme <andreas@pelme.se>
2015-09-26 12:52:18 +0200

    Run tests on pytest 2.8

--- generate_configurations.py
+++ generate_configurations.py
@@ -27,7 +27,7 @@ RUN_PYTHON = '3.4'
 PYTHON_MAIN_VERSIONS = ['python2.7', 'python3.4']
 PYTHON_VERSIONS = ['python2.6', 'python2.7', 'python3.2', 'python3.3',
                    'python3.4', 'pypy', 'pypy3']
-PYTEST_VERSIONS = ['2.7.2']
+PYTEST_VERSIONS = ['2.7.2', '2.8']
 DJANGO_VERSIONS = ['1.3', '1.4', '1.5', '1.6', '1.7', '1.8', 'master']
 SETTINGS = ['sqlite', 'sqlite_file', 'mysql_myisam', 'mysql_innodb',
             'postgres']
