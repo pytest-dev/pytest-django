eef3b96d1ba12c274cb3b143b43795f8f737e2c0
Andreas Pelme <andreas@pelme.se>
2015-09-27 12:19:33 +0200

    Remove master from Travis to get tests for Django 1.9 and all database backends

--- generate_configurations.py
+++ generate_configurations.py
@@ -28,7 +28,7 @@ PYTHON_MAIN_VERSIONS = ['python2.7', 'python3.4']
 PYTHON_VERSIONS = ['python2.6', 'python2.7', 'python3.2', 'python3.3',
                    'python3.4', 'python3.5', 'pypy', 'pypy3']
 PYTEST_VERSIONS = ['2.7.3', '2.8.0']
-DJANGO_VERSIONS = ['1.4', '1.5', '1.6', '1.7', '1.8', '1.9', 'master']
+DJANGO_VERSIONS = ['1.4', '1.5', '1.6', '1.7', '1.8', '1.9',]
 SETTINGS = ['sqlite', 'sqlite_file', 'mysql_myisam', 'mysql_innodb',
             'postgres']
 DJANGO_REQUIREMENTS = {
