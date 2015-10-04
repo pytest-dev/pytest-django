d3e03b90fcfb2be4e0a55bef122291c67a2c22e8
Tomasz Kontusz <tomasz.kontusz@gmail.com>
2015-10-04 09:04:00 +0200

    Keep the existing PYTHONPATH in tox
    Without this the tests could not be run on systems that already use
    PYTHONPATH (like NixOS).

--- generate_configurations.py
+++ generate_configurations.py
@@ -49,7 +49,7 @@ TOX_TESTENV_TEMPLATE = dedent("""
     deps =
     %(deps)s
     setenv =
-         PYTHONPATH = {toxinidir}
+         PYTHONPATH = {toxinidir}:{env:PYTHONPATH:}
          UID = %(uid)s
     """)
 
