pytest-dev/pytest-django

commit 96f5fb1817285fbf9ef9b0de89c5190d134f845a

Fix calling of setUpClass/tearDownClass in classes with multiple inheritance
pytest-django monkeypatches Django's setUpClass / tearDownClass to call
them at the correct time during fixture setup/teardown. The previous
implementation caused problems when used with multiple inheritance.

This commit fixes issue #265.

pytest_django/plugin.py
@@ -230,19 +230,55 @@ def pytest_configure():
     _setup_django()
 
 
-def pytest_runtest_setup(item):
+def _method_is_defined_at_leaf(cls, method_name):
+    return getattr(cls.__base__, method_name).__func__ is not getattr(cls, method_name).__func__
 
+
+_disabled_classmethods = {}
+
+
+def _disable_class_methods(cls):
+    if cls in _disabled_classmethods:
+        return
+
+    _disabled_classmethods[cls] = (
+        cls.setUpClass,
+        _method_is_defined_at_leaf(cls, 'setUpClass'),
+        cls.tearDownClass,
+        _method_is_defined_at_leaf(cls, 'tearDownClass'),
+    )
+
+    cls.setUpClass = types.MethodType(lambda cls: None, cls)
+    cls.tearDownClass = types.MethodType(lambda cls: None, cls)
+
+
+def _restore_class_methods(cls):
+    (setUpClass,
+     restore_setUpClass,
+     tearDownClass,
+     restore_tearDownClass) = _disabled_classmethods.pop(cls)
+
+    try:
+        del cls.setUpClass
+    except AttributeError:
+        raise
+
+    try:
+        del cls.tearDownClass
+    except AttributeError:
+        pass
+
+    if restore_setUpClass:
+        cls.setUpClass = setUpClass
+
+    if restore_tearDownClass:
+        cls.tearDownClass = tearDownClass
+
+
+def pytest_runtest_setup(item):
     if django_settings_is_configured() and is_django_unittest(item):
         cls = item.cls
-
-        if hasattr(cls, '__real_setUpClass'):
-            return
-
-        cls.__real_setUpClass = cls.setUpClass
-        cls.__real_tearDownClass = cls.tearDownClass
-
-        cls.setUpClass = types.MethodType(lambda cls: None, cls)
-        cls.tearDownClass = types.MethodType(lambda cls: None, cls)
+        _disable_class_methods(cls)
 
 
 @pytest.fixture(autouse=True, scope='session')
@@ -315,10 +351,16 @@ def _django_setup_unittest(request, _django_cursor_wrapper):
         request.getfuncargvalue('_django_db_setup')
 
         _django_cursor_wrapper.enable()
-        request.node.cls.__real_setUpClass()
+
+        cls = request.node.cls
+
+        _restore_class_methods(cls)
+        cls.setUpClass()
+        _disable_class_methods(cls)
 
         def teardown():
-            request.node.cls.__real_tearDownClass()
+            _restore_class_methods(cls)
+            cls.tearDownClass()
             _django_cursor_wrapper.restore()
 
         request.addfinalizer(teardown)

tests/test_unittest.py
@@ -119,6 +119,65 @@ class TestUnittestMethods:
         ])
         assert result.ret == 0
 
+    def test_multi_inheritance_setUpClass(self, django_testdir):
+        django_testdir.create_test_module('''
+            from django.test import TestCase
+            from .app.models import Item
+
+            class TestA(TestCase):
+                expected_state = ['A']
+                state = []
+
+                @classmethod
+                def setUpClass(cls):
+                    super(TestA, cls).setUpClass()
+                    cls.state.append('A')
+
+                @classmethod
+                def tearDownClass(cls):
+                    assert cls.state.pop() == 'A'
+                    super(TestA, cls).tearDownClass()
+
+                def test_a(self):
+                    assert self.state == self.expected_state
+
+            class TestB(TestA):
+                expected_state = ['A', 'B']
+
+                @classmethod
+                def setUpClass(cls):
+                    super(TestB, cls).setUpClass()
+                    cls.state.append('B')
+
+                @classmethod
+                def tearDownClass(cls):
+                    assert cls.state.pop() == 'B'
+                    super(TestB, cls).tearDownClass()
+
+                def test_b(self):
+                    assert self.state == self.expected_state
+
+            class TestC(TestB):
+                expected_state = ['A', 'B', 'C']
+
+                @classmethod
+                def setUpClass(cls):
+                    super(TestC, cls).setUpClass()
+                    cls.state.append('C')
+
+                @classmethod
+                def tearDownClass(cls):
+                    assert cls.state.pop() == 'C'
+                    super(TestC, cls).tearDownClass()
+
+                def test_c(self):
+                    assert self.state == self.expected_state
+        ''')
+
+        result = django_testdir.runpytest_subprocess('-vvvv', '-s')
+        assert result.parseoutcomes()['passed'] == 6
+        assert result.ret == 0
+
     def test_unittest(self, django_testdir):
         django_testdir.create_test_module('''
             from unittest import TestCase

pytest_django_test/app/models.py
@@ -3,3 +3,9 @@ from django.db import models
 
 class Item(models.Model):
     name = models.CharField(max_length=100)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name

commit f7e5964ddf19fec05a27b84a85fb22c9566558a0

Fix regression introduced in 96f5fb1817285fbf9ef9b0de89c5190d134f845a.
The previous fix did not properly check the full MRO chain when looking
for the super method, which then failed when used with mixins that does
not define setUpClass/tearDownClass.

This commit fixes issue #280.

pytest_django/plugin.py
@@ -251,7 +251,13 @@ def pytest_configure():
 
 
 def _method_is_defined_at_leaf(cls, method_name):
-    return getattr(cls.__base__, method_name).__func__ is not getattr(cls, method_name).__func__
+    super_method = None
+
+    for base_cls in cls.__bases__:
+        if hasattr(base_cls, method_name):
+            super_method = getattr(base_cls, method_name)
+
+    return getattr(cls, method_name).__func__ is not super_method.__func__
 
 
 _disabled_classmethods = {}

tests/test_unittest.py
@@ -124,7 +124,13 @@ class TestUnittestMethods:
             from django.test import TestCase
             from .app.models import Item
 
-            class TestA(TestCase):
+            # Using a mixin is a regression test, see #280 for more details:
+            # https://github.com/pytest-dev/pytest-django/issues/280
+
+            class SomeMixin(object):
+                pass
+
+            class TestA(SomeMixin, TestCase):
                 expected_state = ['A']
                 state = []
 