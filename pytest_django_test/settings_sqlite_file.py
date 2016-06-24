f0b79e167db9bc204b0432a62874da20047369c3
Andreas Pelme <andreas@pelme.se>
2016-06-24 17:20:46 +0200

    Nicer

--- pytest_django_test/settings_sqlite_file.py
+++ pytest_django_test/settings_sqlite_file.py
@@ -1,21 +1,18 @@
+import tempfile
+import os
+
 from pytest_django_test.settings_base import *  # noqa
 
 # This is a SQLite configuration, which uses a file based database for
 # tests (via setting TEST_NAME / TEST['NAME']).
 
 # The name as expected / used by Django/pytest_django (tests/db_helpers.py).
-import tempfile
-import os
-
-db_name = 'pytest_django' + db_suffix
-test_db_name = 'test_' + db_name
-_tmpdir = tempfile.mkdtemp()
-
+_fd, _filename = tempfile.mkstemp(prefix='test_')
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': '/should_never_be_accessed',
-        'TEST': {'NAME': os.path.join(_tmpdir, test_db_name)},
+        'TEST': {'NAME': _filename},
     },
 }
