ae88e8354872449df72857f63915895c464b52b8
Daniel Hahler <github@thequod.de>
2016-08-20 13:16:09 +0200

    setup.py update setuptools_scm==1.11.1 (#373)

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     license='BSD-3-Clause',
     packages=['pytest_django'],
     long_description=read('README.rst'),
-    setup_requires=['setuptools_scm==1.8.0'],
+    setup_requires=['setuptools_scm==1.11.1'],
     install_requires=['pytest>=2.5'],
     classifiers=['Development Status :: 5 - Production/Stable',
                  'Framework :: Django',
