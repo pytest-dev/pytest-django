e8c8e4e3291a2432e3c4a514f061d27d521d0bd4
Andreas Pelme <andreas@pelme.se>
2016-08-21 21:20:12 +0200

    Remove py26 from the list of trove classifiers.

--- setup.py
+++ setup.py
@@ -37,7 +37,6 @@ setup(
                  'Operating System :: OS Independent',
                  'Programming Language :: Python',
                  'Topic :: Software Development :: Testing',
-                 'Programming Language :: Python :: 2.6',
                  'Programming Language :: Python :: 2.7',
                  'Programming Language :: Python :: 3.2',
                  'Programming Language :: Python :: 3.3',
