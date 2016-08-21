1b3d01ef477666d9dd0c1102766cb55cbeea9e0f
Andreas Pelme <andreas@pelme.se>
2016-08-21 12:41:53 +0200

    Raise a warning when trying to specify a live server port with Django 1.11.

--- tests/test_fixtures.py
+++ tests/test_fixtures.py
@@ -187,6 +187,20 @@ class TestLiveServer:
         with pytest.raises(HTTPError):
             urlopen(live_server + '/static/a_file.txt').read()
 
+    @pytest.mark.skipif(get_django_version() < (1, 11),
+                        reason='Django >= 1.11 required')
+    def test_specified_port_error_message_django_111(self, django_testdir):
+        django_testdir.create_test_module("""
+        def test_with_live_server(live_server):
+            pass
+        """)
+
+        result = django_testdir.runpytest_subprocess('--liveserver=localhost:1234')
+        result.stdout.fnmatch_lines([
+            '*Specifying a live server port is not supported in Django 1.11. This '
+            'will be an error in a future pytest-django release.*'
+        ])
+
 
 @pytest.mark.django_project(extra_settings="""
     AUTH_USER_MODEL = 'app.MyCustomUser'
