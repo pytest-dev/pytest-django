9ff7addb9c2d6f684ca93a059af8900083e15619
Daniel Hahler <git@thequod.de>
2017-10-06 22:25:23 +0200

    Fix test_urls

--- tests/test_urls.py
+++ tests/test_urls.py
@@ -1,11 +1,14 @@
 import pytest
 from django.conf import settings
-from django.core.urlresolvers import is_valid_path
 from django.utils.encoding import force_text
 
 
 @pytest.mark.urls('pytest_django_test.urls_overridden')
 def test_urls():
+    try:
+        from django.urls import is_valid_path
+    except ImportError:
+        from django.core.urlresolvers import is_valid_path
     assert settings.ROOT_URLCONF == 'pytest_django_test.urls_overridden'
     assert is_valid_path('/overridden_url/')
 
