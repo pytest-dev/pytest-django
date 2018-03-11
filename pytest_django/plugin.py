61b987def427d3a0b4a10810cb974f83b741e38c
Simon Kohlmeyer <simon.kohlmeyer@gmail.com>
2018-03-11 18:38:47 +0100

    Remove py dependency
    py is in maintenance mode and there are modern alternatives

--- pytest_django/plugin.py
+++ pytest_django/plugin.py
@@ -11,7 +11,7 @@ import os
 import sys
 import types
 
-import py
+import pathlib
 import pytest
 
 from .django_compat import is_django_unittest  # noqa
@@ -88,13 +88,6 @@ def pytest_addoption(parser):
                   type='bool', default=False)
 
 
-def _exists(path, ignore=EnvironmentError):
-    try:
-        return path.check()
-    except ignore:
-        return False
-
-
 PROJECT_FOUND = ('pytest-django found a Django project in %s '
                  '(it contains manage.py) and added it to the Python path.\n'
                  'If this is wrong, add "django_find_project = false" to '
@@ -121,22 +114,30 @@ def _handle_import_error(extra_message):
 
 
 def _add_django_project_to_path(args):
-    args = [x for x in args if not str(x).startswith("-")]
+    def is_django_project(path):
+        return path.is_dir() and (path / 'manage.py').exists()
 
-    if not args:
-        args = [py.path.local()]
+    def find_django_path(args):
+        args = [pathlib.Path(x) for x in args if not str(x).startswith("-")]
+        args = [p for p in args if p.is_dir()]
 
-    for arg in args:
-        arg = py.path.local(arg)
+        if not args:
+            args = [pathlib.Path.cwd()]
 
-        for base in arg.parts(reverse=True):
-            manage_py_try = base.join('manage.py')
+        for arg in args:
+            if is_django_project(arg):
+                return arg
+            for parent in arg.parents:
+                if is_django_project(parent):
+                    return parent
+        return None
 
-            if _exists(manage_py_try):
-                sys.path.insert(0, str(base))
-                return PROJECT_FOUND % base
-
-    return PROJECT_NOT_FOUND
+    project_dir = find_django_path(args)
+    if project_dir:
+        sys.path.insert(0, str(project_dir))
+        return PROJECT_FOUND % project_dir
+    else:
+        return PROJECT_NOT_FOUND
 
 
 def _setup_django():
