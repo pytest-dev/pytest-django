89fce0d83c9fe8fce3c0a903245f30f4d6209f9c
Daniel Hahler <git@thequod.de>
2018-06-28 09:31:23 +0200

    docs: remove unused static files (css)

--- docs/conf.py
+++ docs/conf.py
@@ -37,11 +37,6 @@ pygments_style = 'sphinx'
 
 html_theme = 'sphinx_rtd_theme'
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'pytest-djangodoc'
 
