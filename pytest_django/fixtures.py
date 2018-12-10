c997befd031778fcd46ffe8d79b02b04edead4dc
Daniel Hahler <git@thequod.de>
2018-12-10 06:45:57 +0100

    Use request.config instead of pytest.config
    Will be deprecated in pytest 4.1.
    Ref: https://github.com/pytest-dev/pytest/commit/b88c3f8f828

--- pytest_django/fixtures.py
+++ pytest_django/fixtures.py
@@ -103,14 +103,14 @@ def django_db_setup(
 
     with django_db_blocker.unblock():
         db_cfg = setup_databases(
-            verbosity=pytest.config.option.verbose,
+            verbosity=request.config.option.verbose,
             interactive=False,
             **setup_databases_args
         )
 
     def teardown_database():
         with django_db_blocker.unblock():
-            teardown_databases(db_cfg, verbosity=pytest.config.option.verbose)
+            teardown_databases(db_cfg, verbosity=request.config.option.verbose)
 
     if not django_db_keepdb:
         request.addfinalizer(teardown_database)
