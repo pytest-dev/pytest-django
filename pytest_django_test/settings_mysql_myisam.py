fe6ddb8c7086accdb9d82746908538a7f805ad70
Daniel Hahler <github@thequod.de>
2019-01-22 11:52:27 +0100

    Travis: use Ubuntu Xenial (#694)
    * tests: mysql: use default_storage_engine
    
    * Travis: update pypy/pypy3 versions
    
    Ref: https://travis-ci.community/t/pypy-2-7-on-xenial/889/4

--- pytest_django_test/settings_mysql_myisam.py
+++ pytest_django_test/settings_mysql_myisam.py
@@ -1,11 +1,11 @@
-from pytest_django_test.settings_base import *  # noqa
+from pytest_django_test.settings_base import *  # noqa: F403
 
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME": "pytest_django" + db_suffix,  # noqa
+        "NAME": "pytest_django" + db_suffix,  # noqa: F405
         "HOST": "localhost",
         "USER": "root",
-        "OPTIONS": {"init_command": "SET storage_engine=MyISAM"},
+        "OPTIONS": {"init_command": "SET default_storage_engine=MyISAM"},
     }
 }
