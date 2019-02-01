c034c07d817f66165009b26bd0c70521ee424b92
Daniel Hahler <git@thequod.de>
2019-02-01 21:35:09 +0100

    Use pkg_resources.parse_version

--- pytest_django/plugin.py
+++ pytest_django/plugin.py
@@ -12,6 +12,7 @@ import sys
 import types
 
 import pytest
+from pkg_resources import parse_version
 
 from .django_compat import is_django_unittest  # noqa
 from .fixtures import django_assert_num_queries  # noqa
@@ -48,6 +49,8 @@ CONFIGURATION_ENV = "DJANGO_CONFIGURATION"
 INVALID_TEMPLATE_VARS_ENV = "FAIL_INVALID_TEMPLATE_VARS"
 
 PY2 = sys.version_info[0] == 2
+
+_handle_unittest_methods = parse_version(pytest.__version__) < parse_version("4.2")
 
 
 # ############### pytest hooks ################
@@ -416,8 +419,8 @@ def _restore_class_methods(cls):
 
 
 def pytest_runtest_setup(item):
-    if pytest.__version__ < "4.2.0":
-        if django_settings_is_configured() and is_django_unittest(item):
+    if django_settings_is_configured() and is_django_unittest(item):
+        if _handle_unittest_methods:
             _disable_class_methods(item.cls)
 
 
@@ -508,7 +511,7 @@ def _django_setup_unittest(request, django_db_blocker):
 
         cls.debug = _cleaning_debug
 
-        if pytest.__version__ < "4.2.0":
+        if _handle_unittest_methods:
             _restore_class_methods(cls)
             cls.setUpClass()
             _disable_class_methods(cls)
