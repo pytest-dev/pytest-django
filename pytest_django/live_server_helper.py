5a32049178284172c738a3028661859447081cb2
Daniel Hahler <git@thequod.de>
2020-01-12 00:01:21 +0100

    remove now unused parse_addr

--- pytest_django/live_server_helper.py
+++ pytest_django/live_server_helper.py
@@ -74,31 +74,3 @@ class LiveServer(object):
 
     def __repr__(self):
         return "<LiveServer listening at %s>" % self.url
-
-
-def parse_addr(specified_address):
-    """Parse the --liveserver argument into a host/IP address and port range"""
-    # This code is based on
-    # django.test.testcases.LiveServerTestCase.setUpClass
-
-    # The specified ports may be of the form '8000-8010,8080,9200-9300'
-    # i.e. a comma-separated list of ports or ranges of ports, so we break
-    # it down into a detailed list of all possible ports.
-    possible_ports = []
-    try:
-        host, port_ranges = specified_address.split(":")
-        for port_range in port_ranges.split(","):
-            # A port range can be of either form: '8000' or '8000-8010'.
-            extremes = list(map(int, port_range.split("-")))
-            assert len(extremes) in (1, 2)
-            if len(extremes) == 1:
-                # Port range of the form '8000'
-                possible_ports.append(extremes[0])
-            else:
-                # Port range of the form '8000-8010'
-                for port in range(extremes[0], extremes[1] + 1):
-                    possible_ports.append(port)
-    except Exception:
-        raise Exception('Invalid address ("%s") for live server.' % specified_address)
-
-    return host, possible_ports
