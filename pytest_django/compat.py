ed04bb0613fb14885efe939d38a7d6ae23a12435
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-08-15 00:01:22 +0200

    Fix support for Django 3.2

--- pytest_django/compat.py
+++ pytest_django/compat.py
@@ -1,8 +1,29 @@
 # This file cannot be imported from until Django sets up
-try:
-    # Django 1.11
-    from django.test.utils import setup_databases, teardown_databases  # noqa: F401
-except ImportError:
+
+
+def _get_setup_and_teardown_databases():
+    try:
+        # Django 3.2+ has added timing capabilities that we don't really support
+        # right now. Unfortunately that new time_keeper is required.
+        from django.test.utils import NullTimeKeeper
+    except ImportError:
+        pass
+    else:
+        from django.test.utils import setup_databases, teardown_databases
+
+        def wrapped_setup_databases(*args, **kwargs):
+            return setup_databases(*args, time_keeper=NullTimeKeeper(), **kwargs)
+
+        return wrapped_setup_databases, teardown_databases
+
+    try:
+        # Django 1.11+
+        from django.test.utils import setup_databases, teardown_databases  # noqa: F401
+    except ImportError:
+        pass
+    else:
+        return setup_databases, teardown_databases
+
     # In Django prior to 1.11, teardown_databases is only available as a method on DiscoverRunner
     from django.test.runner import (  # noqa: F401
         setup_databases,
@@ -13,3 +34,9 @@ except ImportError:
         _DiscoverRunner(verbosity=verbosity, interactive=False).teardown_databases(
             db_cfg
         )
+
+    return setup_databases, teardown_databases
+
+
+setup_databases, teardown_databases = _get_setup_and_teardown_databases()
+del _get_setup_and_teardown_databases
