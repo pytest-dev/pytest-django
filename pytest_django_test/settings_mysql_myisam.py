c7f27cb9a8ad0bd7971c36a795fa5dc7c5d3e40f
Ran Benita <ran@unusedvar.com>
2020-10-16 15:36:41 +0300

    tests: improve mysql settings a bit

--- pytest_django_test/settings_mysql_myisam.py
+++ pytest_django_test/settings_mysql_myisam.py
@@ -2,6 +2,7 @@ from os import environ
 
 from .settings_base import *  # noqa: F401 F403
 
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
@@ -9,6 +10,13 @@ DATABASES = {
         "USER": environ.get("TEST_DB_USER", "root"),
         "PASSWORD": environ.get("TEST_DB_PASSWORD", ""),
         "HOST": environ.get("TEST_DB_HOST", "localhost"),
-        "OPTIONS": {"init_command": "SET default_storage_engine=MyISAM"},
-    }
+        "OPTIONS": {
+            "init_command": "SET default_storage_engine=MyISAM",
+            "charset": "utf8mb4",
+        },
+        "TEST": {
+            "CHARSET": "utf8mb4",
+            "COLLATION": "utf8mb4_unicode_ci",
+        },
+    },
 }
