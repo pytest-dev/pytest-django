924c132e86104e73f76deb784350d272b0ffde47
Tim Gates <tim.gates@iress.com>
2021-08-31 06:14:18 +1000

    docs: Fix a few typos
    There are small typos in:
    - docs/database.rst
    - docs/managing_python_path.rst
    - tests/test_manage_py_scan.py
    
    Fixes:
    - Should read `implicitly` rather than `impliclity`.
    - Should read `implicitly` rather than `implicilty`.
    - Should read `explicitly` rather than `explictly`.

--- tests/test_manage_py_scan.py
+++ tests/test_manage_py_scan.py
@@ -4,9 +4,9 @@ import pytest
 @pytest.mark.django_project(project_root="django_project_root", create_manage_py=True)
 def test_django_project_found(django_testdir) -> None:
     # XXX: Important: Do not chdir() to django_project_root since runpytest_subprocess
-    # will call "python /path/to/pytest.py", which will impliclity add cwd to
+    # will call "python /path/to/pytest.py", which will implicitly add cwd to
     # the path. By instead calling "python /path/to/pytest.py
-    # django_project_root", we avoid impliclity adding the project to sys.path
+    # django_project_root", we avoid implicitly adding the project to sys.path
     # This matches the behaviour when pytest is called directly as an
     # executable (cwd is not added to the Python path)
 
