1f47907a4f075470a80b4ee46404230e678b6f23
Petter Friberg <petter_friberg@hotmail.com>
2024-04-13 08:33:33 +0200

    fixup! Avoid running database migrations for `SimpleTestCase`

--- pytest_django/plugin.py
+++ pytest_django/plugin.py
@@ -560,8 +560,11 @@ def _django_setup_unittest(
 
         if request.cls.databases:
             request.getfixturevalue("django_db_setup")
+            db_unblock = django_db_blocker.unblock()
+        else:
+            db_unblock = contextlib.nullcontext()
 
-        with django_db_blocker.unblock():
+        with db_unblock:
             yield
     finally:
         TestCaseFunction.runtest = original_runtest  # type: ignore[method-assign]
