88ebe7ea33731a70d202d6f2d1523e5b275a1c4c
Javier Buzzi <buzzi.javier@gmail.com>
2025-03-14 13:16:45 -0400

    Update runner.py

--- pytest_django/runner.py
+++ pytest_django/runner.py
@@ -9,28 +9,27 @@ class PytestTestRunner:
     @classmethod
     def add_arguments(cls, parser):
         parser.add_argument(
-            '--keepdb', action='store_true',
-            help='Preserves the test DB between runs.'
+            "--keepdb", action="store_true", help="Preserves the test DB between runs."
         )
 
     def run_tests(self, test_labels, **kwargs):
         """Run pytest and return the exitcode.
 
-        It translates some of Django's test command option to pytest's.
+        It translates some of Django"s test command option to pytest"s.
         """
         import pytest
 
         argv = []
         if self.verbosity == 0:
-            argv.append('--quiet')
+            argv.append("--quiet")
         if self.verbosity == 2:
-            argv.append('--verbose')
+            argv.append("--verbose")
         if self.verbosity == 3:
-            argv.append('-vv')
+            argv.append("-vv")
         if self.failfast:
-            argv.append('--exitfirst')
+            argv.append("--exitfirst")
         if self.keepdb:
-            argv.append('--reuse-db')
+            argv.append("--reuse-db")
 
         argv.extend(test_labels)
         return pytest.main(argv)
