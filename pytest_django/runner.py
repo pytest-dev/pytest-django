e1e21163dd97b0aa81b592c2110a0703507065b4
kingbuzzman <buzzi.javier@gmail.com>
2025-03-17 06:12:01 -0400

    Fix linter

--- pytest_django/runner.py
+++ pytest_django/runner.py
@@ -1,18 +1,28 @@
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from argparse import ArgumentParser
+    from typing import Any
+
+
 class PytestTestRunner:
     """Runs pytest to discover and run tests."""
 
-    def __init__(self, verbosity=1, failfast=False, keepdb=False, **kwargs):
+    def __init__(
+        self, verbosity: int = 1, failfast: bool = False, keepdb: bool = False, **kwargs: Any
+    ) -> None:
         self.verbosity = verbosity
         self.failfast = failfast
         self.keepdb = keepdb
 
     @classmethod
-    def add_arguments(cls, parser):
+    def add_arguments(cls, parser: ArgumentParser) -> None:
         parser.add_argument(
             "--keepdb", action="store_true", help="Preserves the test DB between runs."
         )
 
-    def run_tests(self, test_labels, **kwargs):
+    def run_tests(self, *test_labels: str, **kwargs: Any) -> int:
         """Run pytest and return the exitcode.
 
         It translates some of Django"s test command option to pytest"s.
