60ca81131c1243b3f14c98a1154c0c2595879265
Javier Buzzi <buzzi.javier@gmail.com>
2025-03-31 14:09:17 -0400

    testing the linter v2

--- tests/test_runner.py
+++ tests/test_runner.py
@@ -6,19 +6,17 @@ from pytest_django.runner import TestRunner
 
 
 @pytest.mark.parametrize(
-    (
-        "kwargs, expected",
-        [
-            ({}, call(["tests"])),
-            ({"verbosity": 0}, call(["--quiet", "tests"])),
-            ({"verbosity": 1}, call(["tests"])),
-            ({"verbosity": 2}, call(["-v", "tests"])),
-            ({"verbosity": 3}, call(["-vv", "tests"])),
-            ({"verbosity": 4}, call(["-vvv", "tests"])),
-            ({"failfast": True}, call(["--exitfirst", "tests"])),
-            ({"keepdb": True}, call(["--reuse-db", "tests"])),
-        ],
-    )
+    "kwargs, expected",
+    [
+        ({}, call(["tests"])),
+        ({"verbosity": 0}, call(["--quiet", "tests"])),
+        ({"verbosity": 1}, call(["tests"])),
+        ({"verbosity": 2}, call(["-v", "tests"])),
+        ({"verbosity": 3}, call(["-vv", "tests"])),
+        ({"verbosity": 4}, call(["-vvv", "tests"])),
+        ({"failfast": True}, call(["--exitfirst", "tests"])),
+        ({"keepdb": True}, call(["--reuse-db", "tests"])),
+    ]
 )
 def test_runner_run_tests(monkeypatch, kwargs, expected):
     pytest_mock = Mock()
