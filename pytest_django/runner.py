db0fe7bd0d5dbd89a3201f16f5b96ca8b5e5992f
Javier Buzzi <buzzi.javier@gmail.com>
2025-03-31 13:53:51 -0400

    Apply suggestions from code review
    Co-authored-by: Ran Benita <ran@unusedvar.com>

--- pytest_django/runner.py
+++ pytest_django/runner.py
@@ -2,11 +2,16 @@ from argparse import ArgumentParser
 from typing import Any, Iterable
 
 
-class PytestTestRunner:
-    """Runs pytest to discover and run tests."""
+class TestRunner:
+    """A Django test runner which uses pytest to discover and run tests when using `manage.py test`."""
 
     def __init__(
-        self, verbosity: int = 1, failfast: bool = False, keepdb: bool = False, **kwargs: Any
+        self,
+        *,
+        verbosity: int = 1,
+        failfast: bool = False,
+        keepdb: bool = False,
+        **kwargs: Any,
     ) -> None:
         self.verbosity = verbosity
         self.failfast = failfast
@@ -15,13 +20,18 @@ class PytestTestRunner:
     @classmethod
     def add_arguments(cls, parser: ArgumentParser) -> None:
         parser.add_argument(
+            "--failfast",
+            action="store_true",
+            help="Stops the test suite after the first failure.",
+        )
+        parser.add_argument(
             "--keepdb", action="store_true", help="Preserves the test DB between runs."
         )
 
     def run_tests(self, test_labels: Iterable[str], **kwargs: Any) -> int:
         """Run pytest and return the exitcode.
 
-        It translates some of Django"s test command option to pytest"s.
+        It translates some of Django's test command option to pytest's.
         """
         import pytest
 
