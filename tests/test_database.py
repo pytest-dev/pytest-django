346377ffce9e6cf82dd3bc5c63e8355e1180dd14
kingbuzzman <buzzi.javier@gmail.com>
2025-08-15 08:19:17 -0400

    .

--- tests/test_database.py
+++ tests/test_database.py
@@ -508,7 +508,7 @@ def test_custom_django_db_setup(django_pytester: DjangoPytester) -> None:
             conn = psycopg.connect(
                 user=django_settings.DATABASES[db]['USER'],
                 password=django_settings.DATABASES[db]['PASSWORD'],
-                database="postgres",
+                dbname="postgres",
                 host=django_settings.DATABASES[db]['HOST'],
                 port=django_settings.DATABASES['default']['PORT']
             )
